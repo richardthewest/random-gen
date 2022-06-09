8969be1f1982e99b177f772feda2d381465ac0ed
Richard West <r.j.west@live.com.au>
2022-06-09 22:06:08 +1000

    Minor adjustments to beta Random Movie Gen.

--- js/random-movie.js
+++ js/random-movie.js
@@ -7,17 +7,54 @@ function outputMovie() {
   		return response.text();
 	})
 	.then((text) => {
-        const arr = text.split(/\r?\n/);
-        //test for array
-        //let y = Array.isArray(arr);
+        const arr = text.split(/\r?\n/); //split out unwanted spaces
+        const moviesList = arr.filter (e => e); //remove all empty 'e' spaces
 
-        //random movie generator
-        let arrLength = arr.length;
+        //test for array
+        //let y = Array.isArray(moviesList);
+        
+        //seperate out genre title
+        
+        function genreTitle(a){
+            var ar = [];
+    
+            for(var i = 0; i < a.length; i += 2) {  // take every second element
+                ar.push(a[i]);
+            }
+            return ar.slice(1);
+        }
+        
+
+        //seperate out genre code
+        function genreCode(a){
+            var ar = [];
+    
+            for(var i = 1; i < a.length; i += 2) {  // take every second element, starting at the second ind.
+                ar.push(a[i]);
+            }
+            return ar.slice(1);
+        }
+
+        //test for each array
+        //console.log(genreTitle(moviesList));
+        //console.log(genreCode(moviesList));
+        
+        
+        //create a random number between 1 and movieList.lenth
+        let arrLength = moviesList.length/2;
         var x = Math.floor((Math.random() * arrLength));
 
-        let randomMovie = arr[x];
-        console.log(randomMovie);
-        document.getElementById('randomMovie').innerHTML = randomMovie;
+        let randomMovie = moviesList[x];
+        //console.log('Array Length: ' + arrLength) //get array length
+        //console.log('Random Number: ' + x) //test for x
+
+        //define inner HTML to display
+        let movieGenreTitle = (genreTitle(moviesList)[x]);
+        let movieGenreCode = (genreCode(moviesList)[x]);
+        let webLink = 'www.netflix.com/browse/genre/' + movieGenreCode
+        console.log(webLink);
+
+        //document.getElementById('randomMovie').innerHTML = webLink;
 	});
 };
 
